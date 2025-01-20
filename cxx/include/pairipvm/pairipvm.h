2496a891efa20659f060e072150e2a04b39c1c17
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-01-20 21:59:51 +0100

    Remove C++ code

--- cxx/include/pairipvm/pairipvm.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "pairipvm/types.h"
-#include "pairipvm/context.h"
-#include "pairipvm/insn.h"
-#include "pairipvm/vm.h"
-#include "pairipvm/hash.h"
-#include "pairipvm/ops/opdefs.h"
